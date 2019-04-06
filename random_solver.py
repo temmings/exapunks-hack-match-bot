3a2fba8bdb280309c8f5afc10c67f2d7ad772190
Tetsuya HAMAMOTO <temmings@gmail.com>
2019-04-07 00:11:14 +0900

    WIP

--- random_solver.py
+++ random_solver.py
@@ -7,12 +7,8 @@ from solver import Solver
 class RandomSolver(Solver):
     def solve(self, game: Game):
         n = randrange(game.columns)
-        r = randrange(0, 3)
-        if r == 0:
-            game.char.swap_position(n)
-        elif r == 1:
-            if game.char.having_icon is None:
-                game.char.grab_position(n)
-        elif r == 2:
-            if game.char.having_icon is not None:
-                game.char.throw_position(n)
+        (
+            lambda g, n: g.char.swap_position(n),
+            lambda g, n: g.char.grab_position(n) if game.char.having_icon is None else g.char.throw_position(n),
+            lambda g, n: g.char.throw_position(n) if game.char.having_icon is not None else g.char.grab_position(n),
+        )[randrange(0, 3)](game, n)
